68970f1d8afc784cbc5a493138b7291231925359
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-16 15:35:58 -0300

    Create condicionalelseif.php

---
+++ curso02/condicionalelseif.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nota = 7;
+
+    if ($nota >= 7) {
+        echo "Aprovado(a)!";
+    } else if ($nota >= 5) {
+        echo "De recuperação!";
+    } else {
+        echo "Reprovado(a)!";
+    }
+
+?>
