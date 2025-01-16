a3b9e49239fb36267aba52f0c3986455d120019a
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-16 14:25:11 -0300

    Update index.php

--- curso01/index.php
+++ curso01/index.php
@@ -9,15 +9,25 @@
     <?php
     
         $string = "Curso na DIO";
+        $numeros = 50;
 
     ?>
-    
+
     <h4>
         Essa é uma variável do tipo string, para implementar uma variável do tipo string utilizamos aspas simples ('') ou duplas ("") => 
 
         <?php 
         
             echo $string;
+        
+        ?>
+    </h4>
+    <h4>
+        Essa é uma variável do tipo números/ integer, para implementar essa variável utilizamos somente os números desta forma =>
+
+        <?php 
+        
+            echo $numeros;
         
         ?>
     </h4>
