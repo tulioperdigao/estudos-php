a08f75058bf3fcf337c7d5117e6fa43642aa0a57
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-16 11:24:27 -0300

    Create variavel.php

---
+++ curso01/variavel.php
@@ -0,0 +1,10 @@
+<?php
+    //Entregador de delivery ganha 80 reais por dia porém ainda tem as gorjetas dos clientes.
+
+    $valorDia = 80;
+    $gorjetas = 40;
+    $diaria = $valorDia + $gorjetas;
+
+    echo "Segunda-feira: $diaria";
+    
+?>
