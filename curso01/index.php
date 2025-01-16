29052ccda538272b1b2e59f8a27cc68cd6a664b8
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-16 14:18:30 -0300

    Create index.php

---
+++ curso01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisão PHP</title>
+</head>
+<body>
+    <h4>
+        Essa é uma variável do tipo string, para implementar uma variável do tipo string utilizamos aspas simples ('') ou duplas ("") => $string = "Curso na DIO"
+    </h4>
+</body>
+</html>
