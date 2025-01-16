5f15691afb57dc9c218305bbfaa16b5c7c86d7e9
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-16 15:32:22 -0300

    Create condicionalifelse.php

---
+++ curso02/condicionalifelse.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $nota = 6;
+
+    if ($nota >= 7) {
+        echo "Aluno aprovado!\n";
+    } else {
+        echo "Aluno reprovado!\n";
+    }
+
+    echo $nota >= 7 ? "Aluno(a) aprovado(a)!" : "Aluno(a) reprovado(a)!";
+?>
