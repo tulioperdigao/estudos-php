dcd2f6175ff7435b67db888cbe577d56a8a659ba
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-17 10:36:14 -0300

    Update revisao-array.php

--- curso02/aula02/revisao-array.php
+++ curso02/aula02/revisao-array.php
@@ -1,11 +1,49 @@
 <?php 
 
-    $array = array(
-        "Chave" => "Valor",
-    );
+    $carros = ['Ferrari', 'Bmw', 'Mercedes'];
 
-    $array = [
-        "Chave" => "Valor",
+    $endereco = [
+        'cep' => "00000-000",
+        'numero' => "162",
+        'cidade' => "Belo Horizonte",
+        'estado' => "Minas Gerais"
     ];
+
+    $pessoas = [
+        'pessoa1' => [
+            'Nome' => 'Tulio',
+            'Idade' => 20,
+            'endereco' => [
+                'cep' => "00000-000",
+                'numero' => "162",
+                'cidade' => "Belo Horizonte",
+                'estado' => "Minas Gerais"
+            ]
+        ],
+        'pessoa2' => [
+            'Nome' => 'Luena',
+            'Idade' => 48,
+            'endereco' => [
+                'cep' => "00000-001",
+                'numero' => "70",
+                'cidade' => "São Paulo",
+                'estado' => "São Paulo"
+            ]
+        ],
+    ];
+
+    print_r($carros[2]);
+
+    echo "\n";
+
+    print_r($endereco['cidade']); 
+
+    echo "\n";
+
+    if(isset($pessoas['pessoa2']['endereco']['cidade'])) {
+        print_r($pessoas['pessoa2']['endereco']['cidade']);
+    } else {
+        echo "Chave inválida!";
+    };
 
 ?>
