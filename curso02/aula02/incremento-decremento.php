9652fe51004f8c024832db91750aad8b1c105312
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-01-17 12:06:46 -0300

    Create incremento-decremento.php

---
+++ curso02/aula02/incremento-decremento.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Pós-incremento ";
+$a = 5;
+echo "Deve ser 5: " , $a++, "\n";
+echo "Deve ser 6: ", $a, "\n";
+
+echo "Pré-incremento ";
+$a = 5;
+echo "Deve ser 6: " , ++$a, "\n";
+echo "Deve ser 6: ", $a, "\n";
+
+echo "Pós-decremento ";
+$a = 5;
+echo "Deve ser 5: " , $a--, "\n";
+echo "Deve ser 4: ", $a, "\n";
+
+echo "Pré-decremento ";
+$a = 5;
+echo "Deve ser 4: " , --$a, "\n";
+echo "Deve ser 4: ", $a, "\n";
+
+?>
